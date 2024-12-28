f83f997c30513460499b775159819bc449a8d0a8
김규현 <sam99571@kakao.com>
2024-12-28 13:44:28 +0900

    Create StringFormatting_ex.py

---
+++ StringFormatting_ex.py
@@ -0,0 +1,10 @@
+year = 2024
+subject = '프로그래밍'
+text ='{0}, 김규현의 {1}'.format(year, subject)
+print(text) #출력결과: 2024 김규현의 프로그래밍 
+
+#f-string: 문자열 앞에 f 또는 F를 붙이고 따옴표 안의 중괄호{} 안에 값을 삽입한다
+year=2024
+subject='프로그래밍'
+text=f{year}, 김규현의 {subject}
+print(text) #2024 김규현의 프로그래밍 
